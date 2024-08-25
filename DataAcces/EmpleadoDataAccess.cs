30946fbb0095342c24e803e2a098ed51bdfb061b
Marleny Nataly Arlett Garrido Lázaro <74108713+mArlettGL@users.noreply.github.com>
2024-08-25 13:13:31 -0600

    Add files via upload

---
+++ DataAcces/EmpleadoDataAccess.cs
@@ -0,0 +1,45 @@
+﻿using ProyectoSSASA.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+public class EmpleadoDataAccess
+{
+    private string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+    public void InsertarEmpleado(Empleado empleado)
+    {
+        using (SqlConnection con = new SqlConnection(connectionString))
+        {
+            using (SqlCommand cmd = new SqlCommand("sp_InsertarEmpleado", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Agregar los parámetros correspondientes a la tabla Empleados
+                cmd.Parameters.AddWithValue("@nombre", empleado.Nombre);
+                cmd.Parameters.AddWithValue("@apellido", empleado.Apellido);
+                cmd.Parameters.AddWithValue("@DPI", empleado.DPI);
+                cmd.Parameters.AddWithValue("@sexo", empleado.Sexo);
+                cmd.Parameters.AddWithValue("@fechaNacimiento", empleado.FechaNacimiento);
+                cmd.Parameters.AddWithValue("@direccion", empleado.Direccion ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@NIT", empleado.NIT ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@telefono", empleado.Telefono);
+                cmd.Parameters.AddWithValue("@fechaIngreso", empleado.FechaIngreso);
+                cmd.Parameters.AddWithValue("@sueldo", empleado.Sueldo.HasValue ? (object)empleado.Sueldo.Value : DBNull.Value);
+                cmd.Parameters.AddWithValue("@bonificacion", empleado.Bonificacion.HasValue ? (object)empleado.Bonificacion.Value : DBNull.Value);
+                cmd.Parameters.AddWithValue("@codigoDepartamento", empleado.CodigoDepartamento);
+                cmd.Parameters.AddWithValue("@estatus", empleado.Estatus);
+                cmd.Parameters.AddWithValue("@cargo", empleado.Cargo);
+                cmd.Parameters.AddWithValue("@usuarioCrea", empleado.UsuarioCrea ?? (object)DBNull.Value);
+
+                // Ejecutar el comando
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+        }
+    }
+
+    // Implementa otros métodos como ActualizarEmpleado, ObtenerReporteEmpleados
+
+}
---
+++ Modelo/Empleado.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProyectoSSASA.Models
+{
+    public class Empleado
+    {
+        // Campos obligatorios
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DPI { get; set; }
+        public string Sexo { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public int CodigoDepartamento { get; set; }
+        public int Estatus { get; set; }  // Activo/Inactivo (1/0)
+        public string Cargo { get; set; }
+
+        // Campos opcionales
+        public string Direccion { get; set; }
+        public string NIT { get; set; }
+        public decimal? Sueldo { get; set; }
+        public decimal? Bonificacion { get; set; }
+
+        // Auditoría
+        public DateTime FechaCrea { get; set; }
+        public string UsuarioCrea { get; set; }
+        public DateTime? FechaModifica { get; set; }
+        public string UsuarioModifica { get; set; }
+    }
+}
